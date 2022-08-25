1e1f10e8174823199801f63f0ff5b4458ac6cac5
Samuel Stoltenberg <samuel@taubyte.com>
2022-08-25 16:32:06 -0500

    Add another example

---
+++ code/go/get_websocket_url/get_socket_url.go
@@ -0,0 +1,28 @@
+package lib
+
+import (
+	"bitbucket.org/taubyte/go-sdk/event"
+	"bitbucket.org/taubyte/go-sdk/pubsub"
+)
+
+//export getsocketurl
+func getsocketurl(e event.Event) uint32 {
+	h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+	channel, err := pubsub.Channel("channelToMake")
+	if err != nil {
+		return 1
+	}
+
+	url, err := channel.WebSocket().Url()
+	if err != nil {
+		return 1
+	}
+
+	h.Write([]byte(url.Path))
+
+	return 0
+}
