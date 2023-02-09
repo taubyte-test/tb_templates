ead67ed9dd85cdba9a4cc8d33a0ed5bc3dc7a950
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-02-08 22:18:02 -0600

    Update ping_pong.rs

--- code/functions/Rust/ping_pong/ping_pong.rs
+++ code/functions/Rust/ping_pong/ping_pong.rs
@@ -1,6 +1,13 @@
-// Note, our rust compiler is not yet supported
+extern crate taubyte_rs_sdk;
+use taubyte_rs_sdk::event::events::Event;
 
 
-fn ping() {
-    println!("Pong");
+
+
+#[no_mangle]
+pub unsafe  fn ping(event:Event) -> u32 {
+    let http = event.http().unwrap();
+    _ = http.write("pong".as_bytes()).unwrap();
+    
+    return  0
 }
