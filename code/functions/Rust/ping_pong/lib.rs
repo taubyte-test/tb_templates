7b176dc9e67fdca7a4f07dd52de9d4678cd5a8a8
Samuel Stoltenberg <sam@skelouse.com>
2023-02-24 12:30:18 -0600

    Update for sdk changes and format

--- code/functions/Rust/ping_pong/lib.rs
+++ code/functions/Rust/ping_pong/lib.rs
@@ -1,13 +1,10 @@
-extern crate taubyte_rs_sdk;
-use taubyte_rs_sdk::event::events::Event;
-
-
-
+extern crate taubyte_sdk;
+use taubyte_sdk::event::Event;
 
 #[no_mangle]
-pub unsafe  fn ping(event:Event) -> u32 {
+pub fn ping(event: Event) -> u32 {
     let http = event.http().unwrap();
     _ = http.write("pong".as_bytes()).unwrap();
-    
-    return  0
+
+    return 0;
 }
--- code/smartops/Rust/empty/lib.rs
+++ code/smartops/Rust/empty/lib.rs
@@ -1,4 +1,4 @@
 #[no_mangle]
-pub unsafe  fn function(event:u32) -> u32 {    
-    return  0
+pub fn function(event: u32) -> u32 {
+    return 0;
 }
